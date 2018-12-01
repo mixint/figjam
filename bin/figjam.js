f429b2dad64eec8059ba74b85abbf4dde1a07218
Colten Jackson <jazzyjackson@gmail.com>
2018-12-01 05:18:41 -0600

    saving progress, haven't tested yet

--- bin/figjam.js
+++ bin/figjam.js
@@ -5,40 +5,72 @@ const https = require('https')
 module.exports = class FigJam extends Transflect {
 	constructor(){super()}
 
-	// resolves locally to the module, extensions will load their own default.
-	// right now returns just the one named exactly, in the future,
-	// could do a readdir and return the most likely one: most recent file saved by uid
-	get figroot(query){
-		return `${this.source.query.figroot || this.source.query.fig || ''}.figroot.json`
+	_open(source){
+		// check that I have access to the target directory...
+		return Promise.all([
+			this.loadObject(`${this.source.query.figroot || this.source.query.fig || ''}.figroot.json`)
+			this.loadObject(`${this.source.query.transfig || this.source.query.fig || ''}.transfig.json`)
+			this.loadObject(this.source.query.ctx || this.source.query)
+		]).then([figroot, transfig, ctx] => {
+			this.figroot = figroot
+			this.transfig = transfig
+			thix.ctx = ctx
+		}) // 
 	}
 
-	get transfig(){
-		return this._transfig = this._transfig || this.loadObject(
-			`${this.source.query.transfig || this.source.query.fig || ''}.transfig.json`
-		)
-			// should I check that this exists? do I want to default ? 
-			// I'll just throw an error if you specify nonexistant file.
-			// resolves locally to the module, extensions will load their own default.
+	_end(done){
+		done() // close connection on end unless an extension overrides _end...
 	}
 
-	loadObject(figname){
-		// decide if pathname looks like a url, (has protocol)
+	ctxpush(data, done){
+		// don't overwrite this._open ! just define a .figroot.json in ./conf/
+		// I guess this is where I would inspect the object and look for that 'unique-key=pathname' to know when to send...
+		// let element = Object.entries(this.transfig).pop() // reach into the attributes object of the top level {element: {attributes}} object
+		// 							// TODO do I ever validate this.transfig? ctxify does...
+		// let visibledivID = this.visibledivs[this.transfig[element]['unique-key']]
+		// 	// check if the current object has an attribute by the name given by the unique-key attributes
+		// 	// if so... hide the previous node cotaining that key.
+		// if(visibledivID){
+  //           this.push(ctxify({style: {
+  //               ['#' + visibledivID]: {'display': 'none'}
+  //           }})
+		// }
+
+        
+  //       let id = `x${Math.random().toString(16).slice(2)}`
+
+  //       let template = Object.assign({}, this.transfig, {
+  //       	id: id
+  //       })
+
+		this.push(ctxify(this.transfig, data), done)
+	}
+
+	/**
+	 * @param {(String|Object)} fig - an existing object or path to search for
+	 * Note: Really the only reason to resolve back to an existing object 
+	 * is so I can use loadObject as a promise whether or not query.ctx exists,
+	 * in which case, I just fallback to the pathname query object as my context.
+	 */
+	loadObject(fig){
 		return new Promise((resolve, reject) => {
-			if(!figname) resolve({}) // resolve to empty object if called on undefined param
-			else if(url.query(figname).protocol == 'http'){ // if it looks like a URL...
-				http.get(figname, response => {
+			if(Object.prototype.toString.call(fig) == '[object Object]'){
+				resolve(fig)
+			} else if(url.query(fig).protocol == 'http'){ // if it looks like a URL...
+				http.get(fig, response => {
 					let buffers = []
 					response.on('data', data => buffers.push(data))
 					response.on('end', () => resolve(Buffer.concat(buffers).toString()))
 				})
-			} else if(url.query(figname).protocol == 'https'){
-				https.get(figname, response => {
+			} else if(url.query(fig).protocol == 'https'){
+				https.get(fig, response => {
 					let buffers = []
 					response.on('data', data => buffers.push(data))
 					response.on('end', () => resolve(Buffer.concat(buffers).toString()))
 				})
 			} else {
-				this.find(figname, (err, figfullpath) => {
+				this.find(this.source.pathname, fig, (err, figfullpath) => {
+					if(err) reject(err)
 					fs.readFile(figfullpath, (err, buffer) => {
 						if(err) reject(err)
 						else resolve(buffer.toString())
@@ -71,77 +103,12 @@ module.exports = class FigJam extends Transflect {
 		  // see if we still don't have a fig after this...
 		if(!extantfig){
 			// recursion base case - we've already tried figpath and local directory, no luck:
-			if(pathname == process.env.figpath || '.') callback(new Error(`${basename} not found`))
-			else this.find(process.env.figpath || '.', basename, callback)
+			if(pathname == process.env.figpath || pathname == './conf/') callback(new Error(`${basename} not found`))
+			else this.find(process.env.figpath || './conf/', basename, callback)
 		} else {
 			// else we're done, pass the filepath to callback.
 			callback(null, extantfig)
 		}
-	}
-
-	_open(source){
-		// check that I have access to the target directory...
-		return promiseAccess(source.pathname).then(() => 
-			new Promise(resolve => {
-
-				Promise.all([
-					this.loadObject(this.figroot),
-					this.loadObject(this.transfig),
-					this.loadObject(source.query.ctx || source.query)
-				]).then([figrootObj, transfigObj, ctxObj] => {
-
-				})
-
-				if(url.parse(source.query.ctx || '')
-				// check if there is a ctx 
-				try {
-					url.parse(source.query.ctx)
-				} catch(e){
-					// there is no ctx...
-				}
-
-				try {
-					url.parse(source.query.fig)
-				}
-
-				source.query.ctx
-				source.query.fig / figroot / transfig
-
-
-
-				// well I'm inside a promise so I could iterate throug a promise loop to be nonblocking but, not a priority
-
-				// if that never works, then fallback to local...
-
-
-				// check for figroot=figleaf=transfig= in the current directory, 
-				// should all these confs be present at the top of the cwd?
-				// or available via some kind of path? 
-				// maybe... I check all the complete pathnames implied by the pathname
-				// going to higher directories to get default figs.
-				// .figroot.json could exist in your home directory,
-				// every directory inside your home folder will look like how you want it.
-
-				source.query.fig
-				source.query.figroot = 'name'figroot.json
-				source.query.transfig
-
-			}))
-
-		// search pathname directory for fig.jsons
-		// select most recent file owned by user
-		// render the fig as html
-	}
-
-	_end(done){
-		if(!this.keepAlive){
-			done() // make sure ending the request destroyed the statstream 
-		}
-		// else never close the connection
-	}
-
-	ctxpush(data, done){
-		this.push(ctxify(this.transfig, data), done)
 	}
 
 }
